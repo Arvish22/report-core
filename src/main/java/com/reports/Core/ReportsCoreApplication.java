8b84b78856ce84d7863915fdd5bd65067c8a900b
Anurag <anurag.vishwakarma@srsconsultinginc.com>
2019-04-02 14:27:08 +0530

    intial application setup

---
+++ src/main/java/com/reports/Core/ReportsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.reports.Core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsCoreApplication.class, args);
+	}
+
+}
